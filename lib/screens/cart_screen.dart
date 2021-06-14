e7bf82db05215de23b2ea65785ce4dec4fd2d988
inamullah <inamullah.oraxtech@gmail.com>
2021-06-14 19:12:07 +0500

    adding product detail

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -19,7 +19,7 @@ class CartScreen extends StatelessWidget {
             child: Padding(
               padding: EdgeInsets.all(8),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Text(
                     'Total',
